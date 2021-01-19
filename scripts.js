c83a73f3455cd2a959c314ffabfe5ab22b35b425
aisidoronieto <angel.isinie@gmail.com>
2021-01-19 09:01:43 +0000

    change to https

--- scripts.js
+++ scripts.js
@@ -22,7 +22,7 @@ app.appendChild(container);
 
 
 //API call
-const urlrequest = 'http://aisidoronieto.eu.pythonanywhere.com/qr-api?qrUrl='+ qrUrl;
+const urlrequest = 'https://aisidoronieto.eu.pythonanywhere.com/qr-api?qrUrl='+ qrUrl;
 var request = new XMLHttpRequest();
 request.open('GET', urlrequest, true);
 request.onload = function () {
